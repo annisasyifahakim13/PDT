6cabd9e35c61b56f5894ac76c597d22a8f09a469
Rizka Aprilia <rizkaaprilia567@gmail.com>
2026-06-04 20:48:04 +0700

    p

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
-    <link rel="stylesheet" href="/LostTrack/assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
